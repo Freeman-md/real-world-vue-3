f0188dfbd15063d726a06944c882bfa5dd2a23f1
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-05-14 12:33:55 +0100

    update: viewing event details using dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
+import EventDetailsView from '@/views/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +12,14 @@ const router = createRouter({
       component: EventListView
     },
     {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
+    },
+    {
       path: '/about',
       name: 'about',
-
       component: AboutView
     }
   ]
